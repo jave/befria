fa068c26a2e2845bd6cc8e769570c6444931237e
joakim verona <joakim@verona.se>
2022-12-07 22:34:53 +0100

    improvements for mv3

--- mv3/popup.js
+++ mv3/popup.js
@@ -2,14 +2,34 @@ console.log("JAVE popup ");
 
 browser.storage.onChanged.addListener(changeData => {
     console.log("storage onchanged popup");
-    document.getElementById('blockedCounter').innerHTML =  changeData.unblockCounter.newValue;
+    // //update counter when storage changes
+    // if(changeData.unblockCounter){    document.getElementById('blockedCounter').innerHTML =  changeData.unblockCounter.newValue;}
+    // if(changeData.blocked) {
+    //     if (changeData.blocked.newValue==true)
+    //     { document.getElementById('popupicon').src = "icons/befriad.png" ;}
+    //     else { document.getElementById('popupicon').src = "icons/faengslad.png" ;}
+
+    // }
+    updateUi();
+
 });
+
+//update ui when the popup loads
+
+async function updateUi(){
+    document.getElementById('blockedCounter').innerHTML =  (await chrome.storage.local.get())["unblockCounter"];
+    if ((await chrome.storage.local.get())["blocked"]==true)
+        { document.getElementById('popupicon').src = "icons/befriad.png" ;}
+        else { document.getElementById('popupicon').src = "icons/faengslad.png" ;}
+
+}
+
+updateUi();
 
 
 // send a msg to the bg script
 function handleResponse(message) {
     console.log(`popup: Message from the background script:  ${message.response}   ${message.unblockCounter}`);
-    //alert(`unblock:  ${message.response} ${messsage.unblockCounter}`);
 }
 
 function handleError(error) {
@@ -29,12 +49,6 @@ document.addEventListener("click", (e) => {
     console.log("JAVE popup event handler");
 
     notifyBackgroundPage(e.target.id);
-    // if (e.target.id === "toggleblock") {
-    //     notifyBackgroundPage(e);
-    // }
-
-
-
     e.preventDefault();
 });
 
